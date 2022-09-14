b058d817d709e5a72e8d9559dfe1d619c90647b1
Endalebob <endaleyohannes8@gmail.com>
2022-09-14 23:13:15 +0300

    image changed

--- beautiful_hadiya/lib/profile_page.dart
+++ beautiful_hadiya/lib/profile_page.dart
@@ -5,12 +5,12 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 class Profilepage extends StatelessWidget {
   // const Profilepage({super.key});
   final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+    "images/haddiya1.jpg",
+    "images/haddiya2.jpg",
+    "images/haddiya3.jpg",
+    "images/haddiya4.jpg",
+    "images/haddiya5.jpg",
+    "images/haddiya6.jpg",
   ];
 
   @override
@@ -92,7 +92,7 @@ class _CrouselSliderAnimationState extends State<CrouselSliderAnimation> {
                         child: Container(
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                                image: NetworkImage(widget.images[i]),
+                                image: AssetImage(widget.images[i]),
                                 fit: BoxFit.cover),
                           ),
                         )),
